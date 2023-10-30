75c1fb14f3c8e5703cfe60573f28f330a35eb0a9
juliangoaguirre <143230248+juliangoaguirre@users.noreply.github.com>
2023-10-30 23:08:07 +0000

    incomplete still

---
+++ app.js
@@ -0,0 +1,31 @@
+const sum = (a,b) => {
+    return a + b
+}
+
+console.log(sum(7,3))
+
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+function fromDollarToYen(dollar) {
+let valueInEuro = dollar / oneEuroIs.USD
+let valueInYen = valueInEuro * oneEuroIs.JPY
+return valueInYen
+}
+
+function fromEuroToDollar(euro) {
+let valueInDollar = euro * oneEuroIs.USD
+return valueInDollar
+}
+
+function fromYenToPound(Yen){
+let valueInPound = Yen * oneEuroIs.GBP
+return valueInPound
+}
+
+
+console.log(fromYenToPound(1))
+module.exports = { sum };
